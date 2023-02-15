423d51eea63696ad225eb1af9ae6f35ab3134aed
BerAndrade <111024132+BerAndrade@users.noreply.github.com>
2023-02-15 14:09:57 -0300

    Update conexao.php

--- conexao.php
+++ conexao.php
@@ -1,9 +1,9 @@
 <?php
 
-    define('HOSTNAME','ads_manha2a.mysql.dbaas.com.br');
-    define('HOSTUSER','ads_manha2a');
-    define('HOSTPASSWORD','AdS22Manha!');
-    define('DATABASENAME','ads_manha2a');
+    define('HOSTNAME','...');
+    define('HOSTUSER','...');
+    define('HOSTPASSWORD','...');
+    define('DATABASENAME','...');
 
     $conexao = mysqli_connect(HOSTNAME, HOSTUSER, HOSTPASSWORD, DATABASENAME);
 
